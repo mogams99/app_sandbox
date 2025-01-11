e794def0cdac890f5f86e33747d0a702632d7016
Mochammad Gamal <mogams1811@gmail.com>
2025-01-11 15:49:13 +0700

    chore: :lipstick: create list view builder dynamic value.

--- lib/main_20251101_list_view_dynamic.dart
+++ lib/main_20251101_list_view_dynamic.dart
@@ -50,23 +50,23 @@ class ScrollingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(
-          itemCount: numberList.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              height: 500,
-              decoration: BoxDecoration(
-                color: Colors.greenAccent,
-                border: Border.all(color: Colors.black),
+      body: ListView(
+        children: numberList.map((number) {
+          return Container(
+            height: 100,
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              border: Border.all(color: Colors.white),
+            ),
+            child: Center(
+              child: Text(
+                '$number',
+                style: const TextStyle(fontSize: 45),
               ),
-              child: Center(
-                child: Text(
-                  '${numberList[index]}',
-                  style: const TextStyle(fontSize: 65),
-                ),
-              ),
-            );
-          }),
+            ),
+          );
+        }).toList(),
+      ),
     );
   }
 }
